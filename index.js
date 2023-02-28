9e2b361d9c4b45dd6b82a5fc0e6473d4d21d90d7
laugardc <laugardc@gmail.com>
2023-02-28 15:36:37 -0500

    instalacion test, prueba promesa

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+const mdLinks = (path, options) => {
+  return new Promise((resolve, reject) => {
+    // identificar si la ruta existe
+    if (FileSystem.existsSync(path)){
+      //checar o convertir a una ruta absoluta
+      //probar si esa ruta absoluta es un archivo o un directorio
+      //si es un directorio filtrar los archivos md
+    }else{
+      //si no existe la ruta se rechaza la promesa
+      reject("la ruta no eciste");
+    }
+  });
+}
 module.exports = () => {
-  // ...
+  mdLinks
 };
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,10 +1,13 @@
-const mdLinks = require('../');
+const mdLinks = require('../index.js');
 
 
 describe('mdLinks', () => {
 
   it('should...', () => {
     console.log('FIX ME!');
+  });
+  it('Deberia devolver una promesa', () => {
+    expect(mdLinks).toBe(typeof Promise);
   });
 
 });
